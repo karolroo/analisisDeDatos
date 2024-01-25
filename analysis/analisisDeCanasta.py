ef2c6178610cc05a23b237be731220ab2a0c767d
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-24 20:51:24 -0500

    nt3 Se crea estilos

--- analysis/analisisDeCanasta.py
+++ analysis/analisisDeCanasta.py
@@ -1,17 +1,16 @@
-#1. importar el paquete o paquetes con los que voy a analzar la informacion
-
+#1. Importar el paquete o paquetes con los que voy
+#a analizar la informacion
 import pandas as pd
+from helpers.creacionTabla import crearTabla
 
 def analizarCanastaBasica():
+    #2. Sin importar la fuente (sql, xls, JSON, csv...)
+    #crear una tabla tabulada que se lama DATAFRAME
+    tabla=pd.read_csv('./data/bdcanasta.csv')
+    #print(tabla)
+    crearTabla(tabla,"canastabasica")
 
-#2. Sin importar la fuente (sql, xls, JSON, csv...)
-#Crear una tabla tabulada que se llama DATAFRAME
-    tabla= pd.read_csv("./data/bdcanasta.csv")
+    #3. Aplico filtros para limpiar o seleccionar datos
 
-   # print(tabla)
-#3 Aplico filtros para limpiar o seleccionar datos
-    
-    #print(tabla.head(20)) #Primeros N registros
-    #print(tabla.tail()) #Ultimos regsitros
-
-    print(tabla.describe())
+    #print(tabla.head(20)) #primeros N registros
+    #print(tabla.tail())
