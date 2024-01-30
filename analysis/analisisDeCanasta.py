c7a60fc03967c05a9100299c0f3adaf44c288c62
karolroo <rojaskarol1202@gmail.com>
2024-01-30 16:50:50 -0500

    nt4 Se crea tabla para registros de panes

--- analysis/analisisDeCanasta.py
+++ analysis/analisisDeCanasta.py
@@ -1,16 +1,20 @@
-#1. Importar el paquete o paquetes con los que voy
-#a analizar la informacion
+#1. Importar el paquete o paquetes con los que voy a analisar la informacion
+
 import pandas as pd
-from helpers.creacionTabla import crearTabla
+from helpers.creacionTabla import crearTabla 
 
 def analizarCanastaBasica():
-    #2. Sin importar la fuente (sql, xls, JSON, csv...)
-    #crear una tabla tabulada que se lama DATAFRAME
-    tabla=pd.read_csv('./data/bdcanasta.csv')
+    #2. Sin importar la fuente (sql, xls, JSON ,csv...)
+    #Crear una tabla tabulada que se llama DATAFRAME
+    tabla=pd.read_csv("./data/bdcanasta.csv", skipinitialspace=True)    
     #print(tabla)
-    crearTabla(tabla,"canastabasica")
+    #crearTabla(tabla,"Canastabasica")
+    
+#3. aplico filtro para limpiar o seleccionar datos
 
-    #3. Aplico filtros para limpiar o seleccionar datos
-
-    #print(tabla.head(20)) #primeros N registros
-    #print(tabla.tail())
+    #print(tabla.head(20)) #Primeros N registros
+    #print(tabla.tail()) #ultimos
+    #print(tabla.describe())
+    #filtroPanes=tabla.query("(Producto=='Pan') and (Origen=='India')")
+    filtroPrecios=tabla.query("'Precio por Unidad' < 50")
+    crearTabla(filtroPrecios,"filtroprecios")
--- helpers/creacionTabla.py
+++ helpers/creacionTabla.py
@@ -31,5 +31,5 @@ def crearTabla(dataFrame,nombreTabla):
     
     archivoHTML=archivoHTML.replace('<table border="1" class="dataframe">','<table class="table table-striped">')
     
-    with open(rutaArchivo,"w") as archivo:
+    with open(rutaArchivo,"w", encoding="utf-8") as archivo:
         archivo.write(f"{encabezadoHTML}\n{archivoHTML}\n</body>\n</html>")
